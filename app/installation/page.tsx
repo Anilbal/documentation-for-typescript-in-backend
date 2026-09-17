8013676b182f09bf579ecd20956d07b7e52e01a7
Anil Tamang <anilbal7676@gmail.com>
2026-09-17 22:11:28 +0545

    change package cmd

--- app/installation/page.tsx
+++ app/installation/page.tsx
@@ -41,7 +41,7 @@ export default function InstallationPage() {
             </p>
             <div className="pl-11">
               <pre className="overflow-x-auto rounded-lg border border-zinc-200 bg-zinc-900 p-4 font-mono text-sm text-zinc-100 dark:border-zinc-800">
-                <code>npm init --yes</code>
+                <code>npm init -y</code>
               </pre>
             </div>
           </section>
