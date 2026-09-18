1cbc108410c47c165dc8b3131b6d352efa887565
Anil Tamang <anilbal7676@gmail.com>
2026-09-18 22:47:06 +0545

    remove unused code

--- app/port-defining/page.tsx
+++ app/port-defining/page.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function PortConfigurationPage() {
   return (
     <div className="w-full flex-1 bg-zinc-50 font-sans text-zinc-900 dark:bg-black dark:text-zinc-100">
