30038bcfb4efcf1a61e07e16fd4e8f5fa21c04a2
Anil Tamang <anilbal7676@gmail.com>
2026-09-24 23:43:39 +0545

    fix

--- app/installation/page.tsx
+++ app/installation/page.tsx
@@ -409,7 +409,7 @@ temp/`}
                     href="/port-defining"
                     className="inline-flex shrink-0 items-center justify-center gap-1.5 rounded-md bg-emerald-600 px-3 py-2 font-semibold text-white transition-colors hover:bg-emerald-500 dark:bg-emerald-500 dark:hover:bg-emerald-400 mt-4"
                   >
-                    Learn more about port
+                    Learn more about env
                   </Link>
                 </div>
               </div>
